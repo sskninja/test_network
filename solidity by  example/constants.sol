5825cd3f383722cc6b8e43633468f5703e0f221d
sskninja <saurabhsisodia50582@gmail.com>
2023-07-14 23:33:23 +0530

    counter

---
+++ solidity by  example/constants.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+  pragma solidity ^0.8.20;
+ contract Constants{
+     //coding to uppercase constant variables
+  
+         string public constant symbol = "token";
+         uint public constant totalsupply = 10 ** 9;
+          
+     }
+ 
